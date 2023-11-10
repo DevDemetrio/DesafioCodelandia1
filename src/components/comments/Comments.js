7c5bb853661df5fcdb6118493e9366287b3ae2d3
Devdemetrio <devdemetrio@gmail.com>
2023-11-10 17:20:29 -0400

    Projeto concluido

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -2,16 +2,17 @@ import "./Comments.css";
 
 const Comments = (props) => {
   return (
-    <aside className="comment__container">
-      <header>
-        <div>
-          <span className="data">{props.data}</span>
-          <span className="icon">icone</span>
-        </div>
-        <h2>{props.titulo}</h2>
-      </header>
-
-      <p>{props.texto}</p>
+    <aside>
+      <div className="comment__container">
+        <header>
+          <div>
+            <span className="data">{props.data}</span>
+            <span className="icon">icone</span>
+          </div>
+          <h2>{props.titulo}</h2>
+        </header>
+        <p>{props.texto}</p>
+      </div>
     </aside>
   );
 };
