a12f3c59af9896b896cdc940485a6ee088f343a1
Devdemetrio <devdemetrio@gmail.com>
2024-03-16 21:46:19 -0400

    adicianando api externa

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import Search from "./components/search/Search";
 import Comments from "./components/comments/Comments";
 
 import { useState, useEffect } from "react";
-const url = "http://localhost:3000/noticias";
+const url = "https://api-de-noticias.vercel.app/noticias";
 
 function App() {
   const [news, setNews] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-      const res = await fetch(url);
-      const data = await res.json();
-
-      setNews(data);
+      try {
+        const res = await fetch(url);
+        if (!res.ok) {
+          throw new Error("Failed to fetch");
+        }
+        const data = await res.json();
+        setNews(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     }
     fetchData();
   }, []);
